e629af895981844bd166b95deced99fd524a571a
zjun <565061763@qq.com>
2026-04-29 17:08:28 +0800

    ComposePlayerView支持和ComposePlayer重新绑定策略

--- lib/src/main/java/com/sd/lib/compose/player/ComposePlayerView.kt
+++ lib/src/main/java/com/sd/lib/compose/player/ComposePlayerView.kt
@@ -1,40 +1,62 @@
 package com.sd.lib.compose.player
 
-import androidx.annotation.OptIn
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.media3.common.util.UnstableApi
+import androidx.lifecycle.compose.LifecycleResumeEffect
 import androidx.media3.ui.compose.PlayerSurface
 import androidx.media3.ui.compose.SURFACE_TYPE_SURFACE_VIEW
 import androidx.media3.ui.compose.SURFACE_TYPE_TEXTURE_VIEW
 import androidx.media3.ui.compose.modifiers.resizeWithContentScale
 import androidx.media3.ui.compose.state.rememberPresentationState
 
-@OptIn(UnstableApi::class)
+@SuppressLint("UnsafeOptInUsageError")
 @Composable
 fun ComposePlayerView(
   modifier: Modifier = Modifier,
   player: ComposePlayer,
   contentScale: ContentScale = ContentScale.Fit,
   surfaceType: ComposePlayerViewSurfaceType = ComposePlayerViewSurfaceType.TextureView,
+  rebindKeyProvider: @Composable () -> Any = { },
 ) {
   require(player is PlayerImpl)
 
   val media3Player = player.media3Player
   val presentationState = rememberPresentationState(media3Player)
 
-  PlayerSurface(
-    modifier = modifier.resizeWithContentScale(
-      contentScale = contentScale,
-      sourceSizeDp = presentationState.videoSizeDp,
-    ),
-    player = media3Player,
-    surfaceType = when (surfaceType) {
-      ComposePlayerViewSurfaceType.SurfaceView -> SURFACE_TYPE_SURFACE_VIEW
-      ComposePlayerViewSurfaceType.TextureView -> SURFACE_TYPE_TEXTURE_VIEW
-    },
-  )
+  key(rebindKeyProvider()) {
+    PlayerSurface(
+      modifier = modifier.resizeWithContentScale(
+        contentScale = contentScale,
+        sourceSizeDp = presentationState.videoSizeDp,
+      ),
+      player = media3Player,
+      surfaceType = when (surfaceType) {
+        ComposePlayerViewSurfaceType.SurfaceView -> SURFACE_TYPE_SURFACE_VIEW
+        ComposePlayerViewSurfaceType.TextureView -> SURFACE_TYPE_TEXTURE_VIEW
+      },
+    )
+  }
+}
+
+/**
+ * onResume时，重新绑定[ComposePlayer]和[ComposePlayerView]，
+ * 适用于同一个[ComposePlayer]关联多个[ComposePlayerView]的使用场景
+ */
+@Composable
+fun composePlayerViewRebindKeyOnResume(key: Any? = Unit): Any {
+  var resumeCount by remember(key) { mutableIntStateOf(0) }
+  LifecycleResumeEffect(key) {
+    resumeCount++
+    onPauseOrDispose { }
+  }
+  return resumeCount
 }
 
 enum class ComposePlayerViewSurfaceType {
